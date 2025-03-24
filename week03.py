3688c06845949240e94e4b36a39f788bde04f9e6
shj02 <shj507805@naver.com>
2025-03-24 11:39:12 +0900

    v0.7 array

--- week03.py
+++ week03.py
@@ -1,13 +1,5 @@
-def move_zeros(a_list):
-    zero_index = 0
-    for index, n in enumerate(a_list):
-        if n != 0:
-            a_list[zero_index] = n
-            if zero_index != index:
-                a_list[index] = 0
-            zero_index += 1
-    return(a_list)
+groups = ['HOT', 'Seventeen', 'BND', 'ENHYPEN']
+ratings= [4, 3, 1, 2]
 
-a_list = [8, 0, 3, 0, 12]
-move_zeros(a_list)
-print(a_list)
+group_rating = list(zip(groups, ratings))
+print(group_rating)
