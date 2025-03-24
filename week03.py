56f20c403ba84ec0499addcaba09e40a55931af5
shj02 <shj507805@naver.com>
2025-03-24 12:07:47 +0900

    v1.0 array

--- week03.py
+++ week03.py
@@ -1,22 +1,10 @@
-from traceback import print_tb
+def intersection(l1, l2):
+    l3 = list()
+    for v in l1:
+        if v in l2:
+            l3.append(v)
+    return l3
 
-
-def duplicate_city(cities):
-    result = list()
-    s = set()
-
-    for city in cities:
-        l1 = len(s)
-        s.add(city)
-        l2 = len(s)
-        if l1 == l2: #중복된 값이 들어오는 걸 확인함
-            result.append(city)
-    return result
-
-cities = ['Incheon', 'Incheon', 'Incheon', 'Gimpo', 'Seoul', 'Seoul']
-# cities = {'Incheon', 'Incheon', 'Incheon', 'Gimpo', 'Seoul', 'Seoul'}
-# cities = set(cities)
-cities.append('Anyang')
-cities.append('Seoul')
-print(cities)
-print(duplicate_city(cities))
+l1 = [45, 5, 22, 31, 7, 19]
+l2 = [2, 1, 5, 22, 7, 38, 27, 19, 13, 41]
+print((intersection(l1, l2)))
