a41f797a4ba26b4f3a882e3f6564c142d88bc52d
shj02 <shj507805@naver.com>
2025-03-31 11:23:02 +0900

    v1.3 liked list

--- week04.py
+++ week04.py
@@ -17,8 +17,18 @@ class LinkedList:
             current = current.link  # 다음노드로 이동
         current.link = Node(data)
 
+    def __str__(self):
+        node = self.head
+        out_texts = ""
+        while node is not None:
+            # print(node.data)
+            out_texts = out_texts + str(node.data) + " -> "
+            node = node.link
+        return out_texts + " end"
 
 ll = LinkedList()
 ll.append(8)
 ll.append(10)
 ll.append(-9)
+
+print(ll)
