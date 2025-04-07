92925f8ff2413f5822085e2c8553eae34e7bd32c
shj02 <shj507805@naver.com>
2025-04-07 12:03:48 +0900

    v2.3 stack

--- week05.py
+++ week05.py
@@ -1,12 +1,13 @@
 s1 = list()
 print(len(s1))
-s1.append("Data structure") #push
-s1.append("DataBase") #push
-print(len(s1)) #size
-print(s1[-1])  # peek > ['Data structure', 'DataBase']
-s1.pop()
-print(s1) #['Data structure']
-s1.pop()
-print(s1) #[]
-# s1.pop()
-# print(s1) 더 이상 뺄 게 없어서 오류 발생
+s1.append("Data structure")  # push
+s1.append("DataBase")  # push
+print(len(s1))  # size
+print(s1[-1])  # peek
+print(s1)
+print(s1.pop())
+print(s1)
+print(s1.pop())
+print(s1)
+# print(s1.pop())
+# print(s1)
