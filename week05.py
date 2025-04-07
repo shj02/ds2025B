36ca6a88bdab5fe3884ee5592c8d39727bc84dd3
shj02 <shj507805@naver.com>
2025-04-07 12:20:09 +0900

    v2.4 stack

--- week05.py
+++ week05.py
@@ -1,13 +1,16 @@
-s1 = list()
-print(len(s1))
-s1.append("Data structure")  # push
-s1.append("DataBase")  # push
-print(len(s1))  # size
-print(s1[-1])  # peek
-print(s1)
-print(s1.pop())
-print(s1)
-print(s1.pop())
-print(s1)
-# print(s1.pop())
-# print(s1)
+def check_parentheses(expression : str) -> bool: #type hint
+    stack = []
+    for letter in expression:
+        if letter == "(":
+            stack.append(letter)
+        if letter == ")":
+            if len(stack) == 0:
+                return False
+            else:
+                stack.pop()
+    return len(stack) == 0
+
+print(check_parentheses("(2+3)")) #True
+print(check_parentheses("(2+(3+9))")) #True
+print(check_parentheses("(2+(3+9)")) #False 스택에 여는 소괄호가 하나 남아있음
+print(check_parentheses(")2+(3+9)(")) #False
