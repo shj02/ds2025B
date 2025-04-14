e1e2e2a52b65af085586f6f51d6431bc572d8040
shj02 <shj507805@naver.com>
2025-04-14 12:09:55 +0900

    v2.9 queue

--- week06.py
+++ week06.py
@@ -1,42 +1,7 @@
-class Node:
-    def __init__(self, data, link=None):
-        self.data = data
-        self.link = link
-
-
-class Queue:
-    def __init__(self):
-        self.front = None
-        self.rear = None
-        self.size = 0
-
-    def enqueue(self, data):
-        self.size = self.size + 1
-        node = Node(data)
-        if self.rear is None:
-            self.front = node
-            self.rear = node
-        else:
-            self.rear.link = node
-            self.rear = node
-
-    def dequeue(self):
-        if self.front is None:
-            raise IndexError("Queue is empty!")
-        self.size = self.size - 1
-        temp = self.front
-        # temp.link = None
-        self.front = self.front.link
-        if self.front is None:
-            self.rear = None
-        temp.link = None
-        return temp.data
-
+from queue import Queue
 
 q = Queue()
-q.enqueue("Database")
-q.enqueue("Data structure")
-print(q.size, q.front.data, q.rear.data)
-q.dequeue()
-q.dequeue()
-print(q.size, q.front, q.rear)
+q.put("Database")
+q.put("Data structure")
+print(q.get())
+print(q.get())
