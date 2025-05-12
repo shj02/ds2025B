c89c2dcc9865039089f62ad370fc073a055c6688
shj02 <shj507805@naver.com>
2025-05-12 10:47:53 +0900

    v3.7 tree

--- week10.py
+++ week10.py
@@ -53,17 +53,14 @@ def search(find_number):
     current = root
     while True:
         if find_number == current.data:
-            print(f"{find_number}을(를) 찾았습니다")
-            break
+            return True
         elif find_number < current.data:
             if current.left is None:
-                print(f"{find_number}이(가) 존재하지 않습니다")
-                break
+                return False
             current = current.left
         else:
             if current.right is None:
-                print(f"{find_number}이(가) 존재하지 않습니다")
-                break
+                return False
             current = current.right
 
 
@@ -82,6 +79,9 @@ if __name__ == "__main__":
     pre_order(root)  # 10->8->3->9->15
     print()
 
-    # search 함수에 입력 부분 제거
+    # search 함수에 출력 부분 제거, 리턴 값은 bool
     number = int(input("찾고자 하는 값 : "))
-    search(number)
+    if search(number):
+        print(f"{number}을(를) 찾았습니다")
+    else:
+        print(f"{number}이(가) 존재하지 않습니다")
