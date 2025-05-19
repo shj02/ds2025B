7ad78200d3b3b8724eb144295bea4432d2f664f9
shj02 <shj507805@naver.com>
2025-05-19 11:15:00 +0900

    4.2 graph

--- week11.py
+++ week11.py
@@ -5,6 +5,7 @@ class Graph:
 
 
 G1 = Graph(4)
+G2 = Graph(4)
 
 # 0 == A, 1 == B, 2 == C, 3 == D
 G1.graph[0][1] = 1; G1.graph[0][2] = 1; G1.graph[0][3] = 1
@@ -16,4 +17,15 @@ print("G1 무방향 그래프")
 for r in range(G1.size):
     for c in range(G1.size):
         print(G1.graph[r][c], end=' ')
+    print()
+
+
+# 0 == A, 1 == B, 2 == C, 3 == D
+G2.graph[0][1] = 1; G2.graph[0][2] = 1
+G2.graph[3][0] = 1; G2.graph[3][2] = 1
+
+print("G2 방향 그래프")
+for r in range(G2.size):
+    for c in range(G2.size):
+        print(G2.graph[r][c], end=' ')
     print()
